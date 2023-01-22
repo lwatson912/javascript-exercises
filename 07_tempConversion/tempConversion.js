83710f6bff80d30e4e75885ff21c5a26a71c000e
lwatson912 <lwatson912@gmail.com>
2023-01-22 11:12:12 +0000

    --all

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+    let cel = (farenheit - 32) * .5556;
+    let celRound = cel.toFixed(1);
+    let resultCel = parseFloat(celRound);
+    return resultCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+    let far = (celcius * 1.8) + 32;
+    let farRound = far.toFixed(1);
+    let resultFar = parseFloat(farRound);
+    return resultFar;
+
 };
+
+// (celcius * 1.8) +32 = f
+// (f - 32) * .5556 = c
 
 // Do not edit below this line
 module.exports = {
