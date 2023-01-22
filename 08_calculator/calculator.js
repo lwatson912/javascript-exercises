72fd924b832bc35248348be19e62f5ee2e7f3d72
lwatson912 <lwatson912@gmail.com>
2023-01-22 11:50:26 +0000

    --all

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let arraySum = 0
+  for (let i = 0; i < arr.length; i += 1) {
+    arraySum += arr[i];
+  }
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let arrayMulti = 1;
+  for (let i = 0; i < arr.length; i += 1) {
+    arrayMulti = arrayMulti * arr[i];
+  }
+  return arrayMulti;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+      return (num * factorial(num - 1));
+  }
+
 };
 
 // Do not edit below this line
