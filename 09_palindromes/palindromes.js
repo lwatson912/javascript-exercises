b36e1a8dab8d1a8d3388c9954f02c04e96bd79e7
lwatson912 <lwatson912@gmail.com>
2023-01-22 14:53:21 +0000

    --all

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    textLower = text.toLowerCase();
+    textNoPunc = textLower.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    textNoSpaces = textNoPunc.replace(/\s+/g, '');
+    const myArray = textNoSpaces.split('');
+    myArray.reverse();
+        let newText = myArray.join('');
+        if (newText == textNoSpaces) {
+            return true;
+        }
+        else {
+            return false;
+    }
 };
 
 // Do not edit below this line
