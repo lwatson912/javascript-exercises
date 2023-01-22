bcac812a3e8cc35d0b5f9c55ba0fb3f36fb9984c
lwatson912 <lwatson912@gmail.com>
2023-01-22 22:05:44 +0000

    --all

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 1, b = 0, temp;
+    if (num < 0) {
+        return 'OOPS';
+    }
+    else {
+        while (num > 0) {
+            temp = a;
+            a = a + b;
+            b = temp;
+            num--;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// start with 1, 
+// 1 + 1 = 2
